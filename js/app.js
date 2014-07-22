3bcd59c29aa3d2930c2c7aef9b07650d46a14ce0
Matthew <matthew.padich@gmail.com>
2014-07-22 13:40:20 -0400

    JSON problems

---
+++ js/app.js
@@ -0,0 +1,35 @@
+//go to where main route is defined
+
+//add api call
+
+
+
+self.routes['/api']
+
+var apiDB;
+
+var MongoClient = require('mongoDB').MongoClient;
+
+MongoClient.connect('mongodb://'+connectionString)
+
+
+//ne stuff
+
+coll = apiDB['twitterLinks'];
+coll.find({})
+
+
+function getLatest() {
+
+	$.getJSON("emcade-projdevtest.rhcloud.com", populate),
+
+	function populate(params) {
+
+		var result;
+
+		for( var i in params) {
+
+
+		}
+	}
+}
